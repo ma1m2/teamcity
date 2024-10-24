c55c91e9c2baec49d9ebe5604390df47a7dad63c
other <other@mail.ru>
2024-10-24 17:55:00 +0300

    Add config, spec, base tests

---
+++ src/main/java/com/example/teamcity/api/models/User.java
@@ -0,0 +1,17 @@
+package com.example.teamcity.api.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    String username;
+    String password;
+    //String name;
+    //String email;
+}
