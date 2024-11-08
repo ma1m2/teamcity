38948ca7287ab516599df2ff05c70b4ae3743130
other <other@mail.ru>
2024-11-08 22:16:31 +0300

    UI module Create Project

---
+++ src/test/java/com/example/teamcity/ui/CreateProjectTest.java
@@ -0,0 +1,61 @@
+package com.example.teamcity.ui;
+
+import com.example.teamcity.api.enums.Endpoint;
+import com.example.teamcity.ui.pages.LoginPage;
+import com.example.teamcity.ui.pages.admin.CreateProjectPage;
+import org.testng.annotations.Test;
+
+import static io.qameta.allure.Allure.step;
+
+@Test(groups = {"Regression"})
+public class CreateProjectTest extends BaseUiTest{
+    //private static final String REPO_URL = "https://github.com/AlexPshe/spring-core-for-qa";
+    private static final String REPO_URL = "https://github.com/ma1m2/cucumberJava";
+
+    @Test(description = "User should be able to create Project", groups = {"Positive"})
+    public void userCreatesProject(){
+        // подготовка окружения
+        step("Login as user");
+        loginAs(testData.getUser());
+
+        // взаимодействие с UI
+        step("Open `Create Project Page` (http://localhost:8111/admin/createObjectMenu.html)");
+        step("Send all project parameters (repository URL)");
+        step("Click `Proceed`");
+        step("Fix Project Name and Build Type name values");
+        step("Click `Proceed`");
+        CreateProjectPage.open("_Root")
+                .createForm(REPO_URL)
+                .setupProject(testData.getProject().getName(), testData.getBuildType().getName());
+
+        // проверка состояния API
+        // (корректность отправки данных с UI на API)
+        step("Check that all entities (project, build type) was successfully created with correct data on API level");
+
+        // проверка состояния UI
+        // (корректность считывания данных и отображение данных на UI)
+        step("Check that project is visible on Projects Page (http://localhost:8111/favorite/projects)");
+    }
+
+    @Test(description = "User should not be able to craete project without name", groups = {"Negative"})
+    public void userCreatesProjectWithoutName() {
+        // подготовка окружения
+        step("Login as user");
+        step("Check number of projects");
+
+        // взаимодействие с UI
+        step("Open `Create Project Page` (http://localhost:8111/admin/createObjectMenu.html)");
+        step("Send all project parameters (repository URL)");
+        step("Click `Proceed`");
+        step("Set Project Name");
+        step("Click `Proceed`");
+
+        // проверка состояния API
+        // (корректность отправки данных с UI на API)
+        step("Check that number of projects did not change");
+
+        // проверка состояния UI
+        // (корректность считывания данных и отображение данных на UI)
+        step("Check that error appears `Project name must not be empty`");
+    }
+}
