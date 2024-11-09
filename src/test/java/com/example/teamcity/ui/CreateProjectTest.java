f3509056f0120fab718b003d0df151135ab8ec5a
other <other@mail.ru>
2024-11-09 23:02:23 +0300

    1.12 Complete UI test Create Project

--- src/test/java/com/example/teamcity/ui/CreateProjectTest.java
+++ src/test/java/com/example/teamcity/ui/CreateProjectTest.java
@@ -2,11 +2,14 @@ package com.example.teamcity.ui;
 
 import com.codeborne.selenide.Condition;
 import com.example.teamcity.api.enums.Endpoint;
+import com.example.teamcity.api.generators.TestDataStorage;
 import com.example.teamcity.api.models.Project;
 import com.example.teamcity.ui.pages.ProjectPage;
+import com.example.teamcity.ui.pages.ProjectsPage;
 import com.example.teamcity.ui.pages.admin.CreateProjectPage;
 import org.testng.annotations.Test;
 
+import static com.example.teamcity.api.enums.Endpoint.PROJECTS;
 import static io.qameta.allure.Allure.step;
 
 @Test(groups = {"Regression"})
@@ -32,14 +35,23 @@ public class CreateProjectTest extends BaseUiTest{
 
         // проверка состояния API (корректность отправки данных с UI на API)
         step("Check that all entities (project, build type) was successfully created with correct data on API level");
-        var createdProject = superUserCheckRequests.<Project>getRequest(Endpoint.PROJECTS)
+        var createdProject = superUserCheckRequests.<Project>getRequest(PROJECTS)
                 .read("name:" + testData.getProject().getName());
         softy.assertNotNull(createdProject);
+
+        // add createdProject to TestDataStorage to delete in the end
+        TestDataStorage.getStorage().addCreatedEntity(PROJECTS, createdProject);
 
         // проверка состояния UI (корректность считывания данных и отображение данных на UI)
         step("Check that project is visible on Projects Page (http://localhost:8111/favorite/projects)");
         ProjectPage.open(createdProject.getId())
                 .title.shouldHave(Condition.exactText(testData.getProject().getName()));
+
+        var foundProjects = ProjectsPage.open()
+                .getProjects().stream()
+                .anyMatch(project -> project.getName().text().equals(testData.getProject().getName()));
+
+        softy.assertTrue(foundProjects);
     }
 
     @Test(description = "User should not be able to craete project without name", groups = {"Negative"})
