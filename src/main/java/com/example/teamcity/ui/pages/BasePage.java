62d618c8b5c24c53eccdce69d16798be7325484f
other <other@mail.ru>
2024-11-25 23:41:06 +0300

    SetUp First run TeamCity Server

--- src/main/java/com/example/teamcity/ui/pages/BasePage.java
+++ src/main/java/com/example/teamcity/ui/pages/BasePage.java
@@ -1,4 +1,7 @@
 package com.example.teamcity.ui.pages;
 
+import java.time.Duration;
+
 public abstract class BasePage {
+    protected static final Duration LONG_WAITING = Duration.ofMinutes(3);
 }
